3cd97a1c880abe8009fbdc837d6ff00634b6e29f
atulquest93 <atulquest93@gmail.com>
2019-04-03 00:25:50 +0530

    Added Generic Upload For title & Descriptions

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-const Storage = require('@google-cloud/storage');
+const  Storage  = require('@google-cloud/storage');
+
 
 const Sequelize = require('sequelize');
 const config = require("./../config/config.json");
@@ -8,9 +9,9 @@ const sequelize = new Sequelize(config.dbName, config.userName, config.password 
 	host: config.host,
 	dialect: 'mysql',
 	pool: {
-		max: 5,
+		max: 1000,
 		min: 0,
-		acquire: 30000,
+		acquire: 300000,
 		idle: 10000
 	}
 });
@@ -38,6 +39,41 @@ const GoogleAccounts = sequelize.define('GoogleAccounts', {
 	timestamps: false
 });
 
+const Files = sequelize.define('Files', {
+	id: {
+		type : Sequelize.INTEGER,
+		primaryKey: true,
+		autoIncrement: true,
+	},
+	website: Sequelize.STRING,
+	hotLinkUrl: Sequelize.STRING,
+	bucketName: Sequelize.STRING,
+	type : Sequelize.STRING,
+	bucketId : Sequelize.INTEGER,
+	filename : Sequelize.STRING,
+	hierarchyLevel : Sequelize.STRING,
+	status : Sequelize.STRING,
+	contentType: Sequelize.STRING,
+	size : Sequelize.STRING,
+	createdOn : Sequelize.STRING
+}, {
+	tableName: 'filelist',
+	timestamps: false
+});
+
+const UploadedFiles = sequelize.define('UploadedFiles', {
+	id: {
+		type : Sequelize.INTEGER,
+		primaryKey: true,
+		autoIncrement: true,
+	},
+	fileName: Sequelize.STRING,
+	type: Sequelize.STRING,
+	status: Sequelize.STRING,
+}, {
+	tableName: 'uploadedfiles',
+	timestamps: false
+});
 
 
 router.get('/addWordpress', function(req, res, next) {
@@ -82,14 +118,32 @@ router.get('/getGoogleAccounts', function(req, res, next) {
 
 });
 
+
+router.get('/getFiles', function(req, res, next) {
+	UploadedFiles
+	.findAll({
+		where : {
+			type : req.query.type
+		}
+	})
+	.then(function(err, data) {
+		if (err) {
+			res.json(err);
+		} else {
+			res.json(data);
+		}
+	});
+
+});
+
 var multer  = require('multer')
 var storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    cb(null, 'config/');
-  },
-  filename: function(req, file, cb) {
-    cb(null, file.originalname);
-  }
+	destination: function(req, file, cb) {
+		cb(null, 'config/');
+	},
+	filename: function(req, file, cb) {
+		cb(null, file.originalname);
+	}
 });
 
 var upload = multer({ storage: storage });
@@ -97,22 +151,93 @@ router.post('/uploadServiceKeys', upload.single('files'), function(req, res, nex
 	res.json({"foo":"bar"});
 });
 
-router.get('/refreshStorageFiles', function(req, res, next) {
-
-GoogleAccounts
-	.findOne({
-		where : {
-			id : 13
-		}
+router.post('/uploadFilesGeneric', upload.single('files'), function(req, res, next) {
+	UploadedFiles
+	.create({
+		id: null,
+		fileName: req.body.fileName,
+		type: req.body.type,
+		status: "ACTIVE",
 	})
-	.then(function(err, GoogleAccounts) {
+	.then(function(err, data) {
 		if (err) {
 			res.json(err);
 		} else {
+			res.json(data);
+		}
+	});
+});
 
-			
-						
-			res.json(GoogleAccounts);
+
+
+
+router.get('/refreshStorageFiles', function(req, res, next) {
+
+	GoogleAccounts
+	.findOne({
+		where : {
+			id : 15
+		}
+	})
+	.then(function(data, err) {
+		if (err) {
+			res.json(err);
+		} else {
+			console.log("Starter");
+
+			var storage = Storage({keyFilename: "./config/"+data.fileName});
+			var bucket = data.bucketName;
+			var fileList = [];
+
+			console.log("Start Fetching files ");
+
+			storage
+			.bucket(bucket)
+			.getFiles()
+			.then(results => {
+
+				var data = [];
+				const files = results[0];
+				files.forEach(file => {
+					fileList.push({
+						id : null,
+						website : '',
+						hotLinkUrl : '',
+						bucketName : bucket,
+						type : '',
+						bucketId : 13,
+						filename : file.name,
+						hierarchyLevel : file.name.split("/").length - 1,
+						status : 'ACTIVE',
+						contentType : file.metadata.contentType,
+						size : file.metadata.size,
+						createdOn : file.metadata.timeCreated
+					});
+				});
+
+
+				console.log(fileList.length);
+
+				var size = 500; var arrayOfArrays = [];
+				for (var i=0; i<fileList.length; i+=size) {
+					arrayOfArrays.push(fileList.slice(i,i+size));
+				}
+				console.log(arrayOfArrays);
+
+				arrayOfArrays.forEach(function(item){
+					Files
+					.bulkCreate(item)
+					.then(function(err, GoogleAccounts) {
+					});
+				})
+
+				res.json({status : "Completed"});
+
+			})
+			.catch(err => {
+				console.error('ERROR:', err);
+			});
+
 		}
 	});
 
